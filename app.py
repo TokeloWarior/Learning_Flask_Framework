53c583b2077410e54ad6f93bca3865e5cc9f09bf
Tokelo Makoloane <MKLTOK002@cs.uct.ac.za>
2025-06-25 14:42:27 +0200

    Initial commit - Flask setup

---
+++ app.py
@@ -0,0 +1,16 @@
+
+### Activate the Virtual environment first before running!!!
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Hello Tokelo!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)
